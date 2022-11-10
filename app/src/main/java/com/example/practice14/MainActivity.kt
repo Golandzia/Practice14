1b61b8693e3e02c57d6cfa423963f83cb8dbe9fc
Daniil Golandzia <solovevdaniil01@gmail.com>
2022-11-10 12:08:10 +0300

    final edition

--- app/src/main/java/com/example/practice14/MainActivity.kt
+++ app/src/main/java/com/example/practice14/MainActivity.kt
@@ -5,19 +5,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val arguments: Bundle? = intent.extras
+        val editTextEmailAddress :EditText = findViewById(R.id.editTextEmailAddress)
+        val editTextFullName :EditText = findViewById(R.id.editTextFullName)
+        if(arguments != null){
+            editTextEmailAddress.setText("${arguments.get("emailRedacted")}")
+            editTextFullName.setText("${arguments.get("fullNameRedacted")}")
+        }
     }
     fun sendButton(view: View){
         val editActivity = Intent(this@MainActivity, RedactActivity::class.java)
         //Ниже пример отправления данных в другое Activity
         val editTextEmailAddress :EditText = findViewById(R.id.editTextEmailAddress)
         val editTextFullName :EditText = findViewById(R.id.editTextFullName)
-        intent.putExtra("eMail", editTextEmailAddress.text)
-        intent.putExtra("fullName", editTextFullName.text)
-        startActivity(editActivity)//Запуск другого активити
+        if(editTextEmailAddress.text.isNotEmpty() && editTextFullName.text.isNotEmpty()) {
+            editActivity.putExtra("email", editTextEmailAddress.text.toString())
+            editActivity.putExtra("fullName", editTextFullName.text.toString())
+
+            startActivity(editActivity)//Запуск другого активити
+        }
+        else{
+            val errorToast = Toast.makeText(this, "Введено некорректное значение", Toast.LENGTH_SHORT)
+            errorToast.show()
+        }
+
     }
 }
--- app/src/main/java/com/example/practice14/RedactActivity.kt
+++ app/src/main/java/com/example/practice14/RedactActivity.kt
@@ -1,8 +1,11 @@
 package com.example.practice14
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 
 class RedactActivity : AppCompatActivity() {
 
@@ -10,22 +13,32 @@ class RedactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_redact)
-        showData()
-    }
-        private fun showData(){
         val arguments: Bundle? = intent.extras
         val editTextEmailRedact :EditText = findViewById(R.id.editTextEmailRedact)
         val editTextFullNameRedact :EditText = findViewById(R.id.editTextFullNameRedact)
 
         if(arguments != null){
-            val eMail :String = arguments.getString("eMail", "Ничего не передано")
-            val fullname :String = arguments.getString("fullName", "Ничего не передано")
-            editTextEmailRedact.setText(eMail)
-            editTextFullNameRedact.setText(fullname)
+            editTextEmailRedact.setText("${arguments.get("email")}")
+            editTextFullNameRedact.setText("${arguments.get("fullName")}")
         }
         else{
-            editTextEmailRedact.setText("ERROR")
-            editTextFullNameRedact.setText("ERROR")
+            val errorToast = Toast.makeText(this, "Ошибка передачи данных\nCode:1", Toast.LENGTH_SHORT)
+            errorToast.show()
+        }
+    }
+
+    fun sendRedactedData(view: View){
+        val redactedToMainActivity = Intent(this@RedactActivity, MainActivity::class.java)
+        val editTextEmailRedact :EditText = findViewById(R.id.editTextEmailRedact)
+        val editTextFullNameRedact :EditText = findViewById(R.id.editTextFullNameRedact)
+        if(editTextEmailRedact.text.isNotEmpty() && editTextFullNameRedact.text.isNotEmpty()){
+            redactedToMainActivity.putExtra("emailRedacted", editTextEmailRedact.text.toString() + "*")
+            redactedToMainActivity.putExtra("fullNameRedacted", editTextFullNameRedact.text.toString() + "*")
+            startActivity(redactedToMainActivity)
+        }
+        else{
+            val errorToast = Toast.makeText(this, "Ошибка передачи данных\nCode:2", Toast.LENGTH_SHORT)
+            errorToast.show()
         }
     }
 }
